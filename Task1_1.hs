7d360ab5dcc24b8d53f7ac61116a57a69457fa9d
mazenomran <43556368+mazenomran@users.noreply.github.com>
2019-01-29 21:26:39 +0300

    first try

---
+++ Task1_1.hs
@@ -0,0 +1,51 @@
+module Task1_1 where
+
+import Todo(todo)
+
+data Term = IntConstant{ intValue :: Int }           -- числовая константа
+            | Variable{ varName :: String }          -- переменная
+            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            deriving(Show,Eq)
+
+-- Для бинарных операций необходима не только реализация, но и адекватные
+-- ассоциативность и приоритет
+(|+|) :: Term -> Term -> Term
+(|+|) l r = BinaryTerm Plus l r
+infixl 6 |+|
+(|-|) :: Term -> Term -> Term
+(|-|) l r = BinaryTerm Minus l r
+infixl 6 |-|
+(|*|) :: Term -> Term -> Term
+(|*|) l r =  BinaryTerm Times l r
+infixl 7 |*|
+
+-- Заменить переменную `varName` на `replacement`
+-- во всём выражении `expression`
+replaceVar :: String -> Term -> Term -> Term
+replaceVar varName replacement expression = 
+ let replace hv = replaceVar varName replacement hv in
+        case expression of
+            Variable variable | variable == varName -> replacement
+            BinaryTerm operartion lhv rhv -> BinaryTerm operartion (replace lhv) (replace rhv)
+            _ -> expression 
+-- Посчитать значение выражения `Term`
+-- если оно состоит только из констант
+evaluate :: Term -> Term
+evaluate expression = case expression of
+    BinaryTerm operartion lhs rhs ->
+        case (operartion, left, right) of 
+            (Plus, IntConstant left, IntConstant right) -> IntConstant (left + right)
+            (Minus, IntConstant left, IntConstant right) -> IntConstant (left - right)
+            (Times, IntConstant left, IntConstant right) -> IntConstant (left * right)
+            (Plus, IntConstant 0, right) -> right
+            (Times, IntConstant 1, right) -> right
+            (Times, IntConstant 0, right) -> IntConstant 0
+            (Plus, left, IntConstant 0) -> left
+            (Minus, left, IntConstant 0) -> left
+            (Times, left, IntConstant 0) -> IntConstant 0
+            (Times, left, IntConstant 1) -> left
+            _ -> BinaryTerm operartion left right 
+        where
+            left  = evaluate lhs
+            right = evaluate rhs
+    _ -> expression
